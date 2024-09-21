3eeb7194abe5a4306585374b151a2d85bc1dcbbb
duy <allducduy@gmail.com>
2024-09-21 14:33:15 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+web totinh ne <3
---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Phan Võ Thùy Dương",
+    introTitle: 'Phan Võ Thùy Dương',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'Nhiều thiệt nhiều lun <33333.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: '' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
